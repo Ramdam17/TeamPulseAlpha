f4e608f1c0a753a5be9db7e84b32cc979a0e00a5
remy.ramadour.labs <remy.ramadour.labs@gmail.com>
2024-07-23 00:05:39 +0200

    Initial Commit

---
+++ TeamPulseAlpha/TeamPulseAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamPulseAlphaApp.swift
+//  TeamPulseAlpha
+//
+//  Created by blackstar on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TeamPulseAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeamPulseAlphaTests/TeamPulseAlphaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeamPulseAlphaTests.swift
+//  TeamPulseAlphaTests
+//
+//  Created by blackstar on 23/07/2024.
+//
+
+import Testing
+@testable import TeamPulseAlpha
+
+struct TeamPulseAlphaTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
